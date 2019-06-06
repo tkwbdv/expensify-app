44d1370f3eac1942941cd609009bab57daf3d5d0
ThoKur <Mick.18@gmx.de>
2019-06-06 18:41:13 +0200

    added test response at /test

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const buildPath = path.join(__dirname, "..", "build");
+const port = process.env.PORT || 3001;
+
+app.use(express.static(buildPath));
+
+app.get("/test", (req, res) => {
+  res.send("this is from the server file");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log("Server is up!");
+});
